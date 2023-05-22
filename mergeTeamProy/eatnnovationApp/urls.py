9971e3a96b07bd046b9d69ecfb11b3ab4788e777
lilianmar <lilian.maradiago@gmail.com>
2023-05-22 16:57:36 -0500

    uso de decoradores para restringir acceso a vistas

--- mergeTeamProy/eatnnovationApp/urls.py
+++ mergeTeamProy/eatnnovationApp/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from .views import home,Menu,registro
 from  eatnnovationApp.views import ProductCreate,ProductDelete,ProductDetail,ProductList,ProductUpdate
+from  eatnnovationApp.views import UserList,UserDetail, UserCreate, UserUpdate, UserDelete
 
 urlpatterns = [
     path('', home, name="home"),
-    
+    path('registro/', registro, name='registro'),
     path('menu/', Menu.as_view(template_name = "eatnnovationApp/menu.html"), name='menu'),
     # La ruta 'leer' en donde listamos todos los registros o Products de la Base de Datos
+
+
     path('productList/', ProductList.as_view(), name='productList'),
     # La ruta 'details' en donde mostraremos una página con los details de un Products o registro 
     path('productList/detail/<int:pk>', ProductDetail.as_view(template_name = "eatnnovationApp/detail_product.html"), name='detailProduct'),
@@ -16,7 +19,16 @@ urlpatterns = [
     path('productList/edit/<int:pk>', ProductUpdate.as_view(template_name = "eatnnovationApp/update_product.html"), name='updateProduct'), 
     # La ruta 'delete' que usaremos para delete un Products o registro de la Base de Datos 
     path('productList/delete/<int:pk>', ProductDelete.as_view(), name='deleteProduct'),
-    path('registro/', registro, name='registro'),
+    
+
+    path('userList/', UserList.as_view(template_name = "eatnnovationApp/user_list.html"), name='userList'),
+    path('userList/detail/<int:pk>', UserDetail.as_view(template_name = "eatnnovationApp/detail_user.html"), name='detailUser'),
+    # La ruta 'create' en donde mostraremos un formulario para create un nuevo Products o registro  
+    path('userList/create', UserCreate.as_view(template_name = "eatnnovationApp/create_user.html"), name='createUser'),
+    # La ruta 'actualizar' en donde mostraremos un formulario para actualizar un Products o registro de la Base de Datos 
+    path('userList/edit/<int:pk>', UserUpdate.as_view(template_name = "eatnnovationApp/update_user.html"), name='updateUser'), 
+    # La ruta 'delete' que usaremos para delete un Products o registro de la Base de Datos 
+    path('userList/delete/<int:pk>', UserDelete.as_view(), name='deleteUser'),
 
 
 ]
--- mergeTeamProy/eatnnovationApp/views.py
+++ mergeTeamProy/eatnnovationApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import get_user_model
 from django.views.generic import ListView, DetailView 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 #Nos sirve para redireccionar despues de una acción revertiendo patrones de expresiones regulares 
@@ -10,56 +11,18 @@ from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth import authenticate, login
 from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import user_passes_test
+from django.utils.decorators import method_decorator
 
 
+def is_staff(user):
+    return user.is_authenticated and user.is_staff
 # Create your views here.
 def home (request):
     return render(request, 'eatnnovationApp/home.html')
 
 def menu (request):
     return render(request, 'eatnnovationApp/menu.html')
-
-class Menu (ListView):
-    model = Product 
-
-
-class ProductList(ListView):
-    model = Product 
-
-class ProductCreate(SuccessMessageMixin, CreateView): 
-    model = Product # Llamamos a la clase 'Product' que se encuentra en nuestro archivo 'models.py'
-    form = Product # Definimos nuestro formulario con el nombre de la clase o modelo 'Product'
-    fields = "__all__" # Le decimos a Django que muestre todos los campos de la tabla 'products' de nuestra Base de Datos 
-    success_message = 'Product Created Succesfully!' # Mostramos este Mensaje luego de Crear un Product
-
-    # Redireccionamos a la página principal luego de crear un registro o Product
-    def get_success_url(self):        
-        return reverse('productList') # revisar la pagina a la que se va a redireccionar User 
-      
-class ProductDetail(DetailView): 
-    model = Product # Llamamos a la clase 'Product' que se encuentra en nuestro archivo 'models.py' 
-
-class ProductUpdate(SuccessMessageMixin, UpdateView): 
-    model = Product # Llamamos a la clase 'Product' que se encuentra en nuestro archivo 'models.py' 
-    form = Product # Definimos nuestro formulario con el nombre de la clase o modelo 'Product' 
-    fields = "__all__" # Le decimos a Django que muestre todos los campos de la tabla 'products' de nuestra Base de Datos 
-    success_message = 'Product Updated Succesfully !' # Mostramos este Mensaje luego de Editar un Product 
-
-    # Redireccionamos a la página principal luego de actualizar un registro o Product
-    def get_success_url(self):               
-        return reverse('productList') # Redireccionamos a la vista principal 'leer'
-
-class ProductDelete(SuccessMessageMixin, DeleteView): 
-    model = Product 
-    form = Product
-    fields = "__all__"     
-
-    # Redireccionamos a la página principal luego de eliminar un registro o Product
-    def get_success_url(self): 
-        success_message = 'Product deleted Succesfully !' # Mostramos este Mensaje luego de Editar un Product 
-        messages.success (self.request, (success_message))       
-        return reverse('productList') # Redireccionamos a la vista principal 'leer'  
-    
 
 def registro(request):
     data = {
@@ -72,9 +35,103 @@ def registro(request):
             formulario.save()
             user = authenticate(username=formulario.cleaned_data["username"],password=formulario.cleaned_data["password1"])
             login(request, user)
-            messages.success(request, "Te has registrado correctamente")
+            messages.success(request, "Successful registration")
             return redirect(reverse('home'))
         data['form'] = formulario
 
 
     return render(request, 'registration/registro.html', data)
+
+
+class Menu (ListView):
+    model = Product 
+
+@method_decorator(user_passes_test(is_staff), name='dispatch')
+class ProductList(ListView):
+    model = Product 
+
+@method_decorator(user_passes_test(is_staff), name='dispatch')
+class ProductCreate(SuccessMessageMixin, CreateView): 
+    model = Product # Llamamos a la clase 'Product' que se encuentra en nuestro archivo 'models.py'
+    form = Product # Definimos nuestro formulario con el nombre de la clase o modelo 'Product'
+    fields = "__all__" # Le decimos a Django que muestre todos los campos de la tabla 'products' de nuestra Base de Datos 
+    success_message = 'Product Created Succesfully!' # Mostramos este Mensaje luego de Crear un Product
+
+    # Redireccionamos a la página principal luego de crear un registro o Product
+    def get_success_url(self):        
+        return reverse('productList') # revisar la pagina a la que se va a redireccionar User 
+
+@method_decorator(user_passes_test(is_staff), name='dispatch')      
+class ProductDetail(DetailView): 
+    model = Product # Llamamos a la clase 'Product' que se encuentra en nuestro archivo 'models.py' 
+
+@method_decorator(user_passes_test(is_staff), name='dispatch')
+class ProductUpdate(SuccessMessageMixin, UpdateView): 
+    model = Product # Llamamos a la clase 'Product' que se encuentra en nuestro archivo 'models.py' 
+    form = Product # Definimos nuestro formulario con el nombre de la clase o modelo 'Product' 
+    fields = "__all__" # Le decimos a Django que muestre todos los campos de la tabla 'products' de nuestra Base de Datos 
+    success_message = 'Product Updated Succesfully !' # Mostramos este Mensaje luego de Editar un Product 
+
+    # Redireccionamos a la página principal luego de actualizar un registro o Product
+    def get_success_url(self):               
+        return reverse('productList') # Redireccionamos a la vista principal 'leer'
+
+@method_decorator(user_passes_test(is_staff), name='dispatch')
+class ProductDelete(SuccessMessageMixin, DeleteView): 
+    model = Product 
+    form = Product
+    fields = "__all__"     
+
+    # Redireccionamos a la página principal luego de eliminar un registro o Product
+    def get_success_url(self): 
+        success_message = 'Product deleted Succesfully !' # Mostramos este Mensaje luego de Editar un Product 
+        messages.success (self.request, (success_message))       
+        return reverse('productList') # Redireccionamos a la vista principal 'leer'  
+    
+
+
+
+User = get_user_model()
+
+@method_decorator(user_passes_test(is_staff), name='dispatch')
+class UserList(ListView):
+    model = User 
+
+@method_decorator(user_passes_test(is_staff), name='dispatch')
+class UserCreate(SuccessMessageMixin, CreateView): 
+    model = User # Llamamos a la clase User que se encuentra en nuestro archivo 'models.py'
+    form = User # Definimos nuestro formulario con el nombre de la clase o modelo User
+    fields = ['username', 'email','is_superuser', 'is_staff', 'password'] # Le decimos a Django que muestre todos los campos de la tabla users de nuestra Base de Datos 
+    success_message = 'User Created Succesfully!' # Mostramos este Mensaje luego de Crear un Product
+
+    # Redireccionamos a la página principal luego de crear un registro o User
+    def get_success_url(self):        
+        return reverse('userList') 
+      
+@method_decorator(user_passes_test(is_staff), name='dispatch')
+class UserDetail(DetailView): 
+    model = User # Llamamos a la clase User que se encuentra en nuestro archivo 'models.py' 
+
+@method_decorator(user_passes_test(is_staff), name='dispatch')
+class UserUpdate(SuccessMessageMixin, UpdateView): 
+    model = User # Llamamos a la clase User que se encuentra en nuestro archivo 'models.py' 
+    form = User # Definimos nuestro formulario con el nombre de la clase o modelo User 
+    fields = ['username', 'email','is_superuser', 'is_staff', 'password'] # Le decimos a Django que muestre todos los campos de la tabla users de nuestra Base de Datos 
+    success_message = 'User Updated Succesfully !' # Mostramos este Mensaje luego de Editar un User 
+
+    # Redireccionamos a la página principal luego de actualizar un registro o User
+    def get_success_url(self):               
+        return reverse('userList') # Redireccionamos a la vista principal 'leer'
+
+@method_decorator(user_passes_test(is_staff), name='dispatch')
+class UserDelete(SuccessMessageMixin, DeleteView): 
+    model = User 
+    form = User
+    fields = "__all__"     
+
+    # Redireccionamos a la página principal luego de eliminar un registro o User
+    def get_success_url(self): 
+        success_message = 'User deleted Succesfully !' # Mostramos este Mensaje luego de Editar un User 
+        messages.success (self.request, (success_message))       
+        return reverse('userList') # Redireccionamos a la vista principal 'leer'  
+    

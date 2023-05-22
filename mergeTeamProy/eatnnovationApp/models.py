7e883c1e786a53e065abe9eddbc0e4c61b00dd76
Ngonzalez693 <nicogonto@gmail.com>
2023-05-22 03:10:17 -0500

    menu para admin y clientes

---
+++ mergeTeamProy/eatnnovationApp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+class Meta:
+     db_table = 'categories' 
+     
+
+class Product(models.Model):
+    category=models.ForeignKey(Category, on_delete=models.PROTECT)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    img=models.ImageField(upload_to="products", null=True)
+    price = models.IntegerField()
+    availableCant = models.IntegerField()
+    def __str__(self):
+        return self.name
+class Meta:
+     db_table = 'products' 
---
+++ mergeTeamProy/eatnnovationApp/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render
+from .models import Product
+from django.views.generic import ListView, DetailView 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+#Nos sirve para redireccionar despues de una acción revertiendo patrones de expresiones regulares 
+from django.urls import reverse
+#Habilitamos el uso de mensajes en Django 
+from django.contrib import messages
+#Habilitamos los mensajes para class-based views 
+from django.contrib.messages.views import SuccessMessageMixin
+
+
+# Create your views here.
+def home (request):
+    return render(request, 'eatnnovationApp/home.html')
+
+def menu (request):
+    productos = Product.objects.all()
+    data = {
+        'productos' : productos
+    }
+
+    return render(request, 'eatnnovationApp/menu.html', data)
+
+class Menu (ListView):
+    model = Product 
+
+
+class ProductList(ListView):
+    model = Product 
+
+class ProductCreate(SuccessMessageMixin, CreateView): 
+    model = Product # Llamamos a la clase 'Product' que se encuentra en nuestro archivo 'models.py'
+    form = Product # Definimos nuestro formulario con el nombre de la clase o modelo 'Product'
+    fields = "__all__" # Le decimos a Django que muestre todos los campos de la tabla 'products' de nuestra Base de Datos 
+    success_message = 'Product Created Succesfully!' # Mostramos este Mensaje luego de Crear un Product
+
+    # Redireccionamos a la página principal luego de crear un registro o Product
+    def get_success_url(self):        
+        return reverse('productList') # revisar la pagina a la que se va a redireccionar User 
+      
+class ProductDetail(DetailView): 
+    model = Product # Llamamos a la clase 'Product' que se encuentra en nuestro archivo 'models.py' 
+
+class ProductUpdate(SuccessMessageMixin, UpdateView): 
+    model = Product # Llamamos a la clase 'Product' que se encuentra en nuestro archivo 'models.py' 
+    form = Product # Definimos nuestro formulario con el nombre de la clase o modelo 'Product' 
+    fields = "__all__" # Le decimos a Django que muestre todos los campos de la tabla 'products' de nuestra Base de Datos 
+    success_message = 'Product Updated Succesfully !' # Mostramos este Mensaje luego de Editar un Product 
+
+    # Redireccionamos a la página principal luego de actualizar un registro o Product
+    def get_success_url(self):               
+        return reverse('productList') # Redireccionamos a la vista principal 'leer'
+
+class ProductDelete(SuccessMessageMixin, DeleteView): 
+    model = Product 
+    form = Product
+    fields = "__all__"     
+
+    # Redireccionamos a la página principal luego de eliminar un registro o Product
+    def get_success_url(self): 
+        success_message = 'Product deleted Succesfully !' # Mostramos este Mensaje luego de Editar un Product 
+        messages.success (self.request, (success_message))       
+        return reverse('productList') # Redireccionamos a la vista principal 'leer'  
+    
